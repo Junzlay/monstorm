ebeab3f85c5583ce826b25074d60d0db55557308
Junzlay <junzlay@gmail.com>
2022-05-21 17:42:55 +0800

    apex chart

--- js/72.d8397b9f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunkmonstorm_fe"]=self["webpackChunkmonstorm_fe"]||[]).push([[72],{7072:(n,e,s)=>{s.r(e),s.d(e,{default:()=>d});var t=s(3673);function o(n,e,s,o,r,a){const c=(0,t.up)("q-page");return(0,t.wg)(),(0,t.j4)(c,{padding:""})}const r={};var a=s(4260),c=s(4379),u=s(7518),p=s.n(u);const f=(0,a.Z)(r,[["render",o]]),d=f;p()(r,"components",{QPage:c.Z})}}]);
